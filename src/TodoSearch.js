44a9f207b9369e34dfb95c3298453a188869f26a
jaracar84 <cajfdeveloper@gmail.com>
2024-02-15 16:31:25 -0500

    file create | project setup and Todo in progress

---
+++ src/TodoSearch.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TodoSearch.css";
+
+function TodoSearch({searchValue,setSearchValue}) {
+  return (
+    <input 
+      placeholder="Filter TODOs" 
+      className="TodoSearch" 
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
+}
+
+export { TodoSearch };
